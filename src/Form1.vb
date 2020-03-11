b77dff99cf4991b10d6b9df99a5af8d3623e7e35
Strappazzon <strappazzon@protonmail.com>
2020-03-11 16:59:02 +0100

    Parse command line arguments

--- src/Form1.vb
+++ src/Form1.vb
@@ -1,11 +1,33 @@
 ﻿Imports System.ComponentModel
 Imports System.IO
 Imports System.Net
+Imports MatthiWare.CommandLine
 Imports ZNix.SuperBLT
 
 Public Class Form1
     Private Const VersionCode As Short = 1
     Private WithEvents HashInput As New BackgroundWorker()
+
+    Private Sub ParseCmdArgs()
+        'Parse command line arguments
+        '//www.youtube.com/watch?v=mObWcaigMTU
+        Dim CmdArguments = New CommandLineParser(Of Arguments)().Parse(Environment.GetCommandLineArgs)
+
+        If Not CmdArguments.HasErrors Then
+            PathTextBox.Text = (CmdArguments.Result.InputPath).Trim(""""c, "") 'Trim quotation marks
+            CopyToHashTxtCheckBox.Checked = CmdArguments.Result.ExportHash
+            CopyToClipboardCheckBox.Checked = CmdArguments.Result.ClipboardCopy
+        End If
+
+        If CmdArguments.HelpRequested Then
+            MessageBox.Show("Usage: " & Path.GetFileName(Application.ExecutablePath).Replace(".exe", "") & " [options]" & Environment.NewLine & Environment.NewLine &
+                                "-p, --path         Path to folder or file you want to hash." & Environment.NewLine &
+                                "-e, --export       Write computed hash to a text file." & Environment.NewLine &
+                                "-c, --clipboard  Copy computed hash to the clipboard." & Environment.NewLine & Environment.NewLine &
+                                "Please consult the Readme file for more information.",
+                                Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
 
     Private Sub UpgradeSettings()
         'Migrate settings to the new version
@@ -18,7 +40,7 @@ Public Class Form1
     End Sub
 
     Private Sub HashInput_DoWork(sender As Object, e As DoWorkEventArgs) Handles HashInput.DoWork
-        'Check whether the dropped item is a folder or a file
+        'Check whether the specified/dropped path is a folder or a file
         '//stackoverflow.com/a/439478
         Dim IsDir As Boolean = (File.GetAttributes(PathTextBox.Text) And FileAttributes.Directory) = FileAttributes.Directory
 
@@ -34,7 +56,26 @@ Public Class Form1
     End Sub
 
     Private Sub HashInput_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles HashInput.RunWorkerCompleted
+        'Write the computed hash
         HashTextBox.Text = e.Result()
+
+        'Write the hash to a text file
+        If CopyToHashTxtCheckBox.Checked = True Then
+            Using S As New SaveFileDialog
+                S.Title = "Save hash as..."
+                S.InitialDirectory = PathTextBox.Text
+                S.FileName = "hash"
+                S.Filter = "Text file|.txt"
+                If S.ShowDialog = DialogResult.OK Then
+                    File.AppendAllText(S.FileName, e.Result())
+                End If
+            End Using
+        End If
+
+        'Copy the hash to clipboard
+        If CopyToClipboardCheckBox.Checked = True Then
+            Clipboard.SetText(e.Result())
+        End If
     End Sub
 
     Private Sub Updater_DownloadStringCompleted(ByVal sender As Object, ByVal e As DownloadStringCompletedEventArgs)
@@ -61,6 +102,9 @@ Public Class Form1
         CopyToClipboardCheckBox.Checked = My.Settings.CopyHashToClipboard
         UpdatesCheckBox.Checked = My.Settings.CheckUpdates
 
+        'Parse command line arguments
+        ParseCmdArgs()
+
         'Check for updates
         If UpdatesCheckBox.Checked = True Then
             Using Updater As New WebClient
@@ -76,6 +120,12 @@ Public Class Form1
         ToolTips.OwnerDraw = True
         ToolTips.ForeColor = Color.FromArgb(255, 153, 153, 153)
         ToolTips.BackColor = Color.FromArgb(255, 17, 17, 17)
+
+        'Hash the input from command line argument
+        If PathTextBox.Text <> "" Then
+            HashTextBox.Text = "Computing..."
+            HashInput.RunWorkerAsync()
+        End If
     End Sub
 
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
@@ -104,24 +154,6 @@ Public Class Form1
             'Hash the input
             HashTextBox.Text = "Computing..."
             HashInput.RunWorkerAsync()
-
-            'Write the hash to a text file
-            If CopyToHashTxtCheckBox.Checked = True Then
-                Using S As New SaveFileDialog
-                    S.Title = "Save hash as..."
-                    S.InitialDirectory = PathTextBox.Text
-                    S.FileName = "hash"
-                    S.Filter = "Text file|.txt"
-                    If S.ShowDialog = DialogResult.OK Then
-                        File.AppendAllText(S.FileName, HashTextBox.Text)
-                    End If
-                End Using
-            End If
-
-            'Copy the hash to clipboard
-            If CopyToClipboardCheckBox.Checked = True Then
-                Clipboard.SetText(HashTextBox.Text)
-            End If
         Else
             MessageBox.Show("Please drop one folder or file at a time.", Text, MessageBoxButtons.OK, MessageBoxIcon.Warning)
         End If
